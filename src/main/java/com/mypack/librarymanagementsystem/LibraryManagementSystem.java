c9065490aeec4bc851ea84c0bfab0376dbe65323
Bipul Pal <bipulpalkatwa@gmail.com>
2023-09-28 18:13:20 +0530

    Initial commit

---
+++ src/main/java/com/mypack/librarymanagementsystem/LibraryManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mypack.librarymanagementsystem;
+
+/**
+ *
+ * @author dhira
+ */
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
